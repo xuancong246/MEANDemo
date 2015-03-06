4dd2278282b8963196e23fe9588e64953afc22c9
osd_cong <cong.pham@orientsoftware.net>
2015-03-06 15:06:49 +0700

    Filter for ng-grid

--- public/app/course/using-ng-grid/mvCourseListNgGridCtrl.js
+++ public/app/course/using-ng-grid/mvCourseListNgGridCtrl.js
@@ -27,71 +27,71 @@
     }
 
     $scope.filterOptions = {
-        filterText: "",
-        useExternalFilter: true
+      filterText: "",
+      useExternalFilter: true
     };
     $scope.totalServerItems = 0;
     $scope.pagingOptions = {
-        pageSizes: [5, 10, 20],
-        pageSize: 5,
-        currentPage: 1
+      pageSizes: [5, 10, 20],
+      pageSize: 5,
+      currentPage: 1
     };
+
     $scope.setPagingData = function(data, page, pageSize){
-        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-        $scope.myData = pagedData;
-        $scope.totalServerItems = data.length;
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
+      var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+      $scope.myData = pagedData;
+      $scope.totalServerItems = data.length;
+      if (!$scope.$$phase) {
+        $scope.$apply();
+      }
     };
+
     $scope.getPagedDataAsync = function (pageSize, page, searchText) {
-        setTimeout(function () {
-            var data;
-            if (searchText) {
-                var ft = searchText.toLowerCase();
-                $http.get('./app/course/using-ng-grid/largeLoad.json').success(function (largeLoad) {
-                    data = largeLoad.filter(function(item) {
-                        return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
-                    });
-                    $scope.setPagingData(data,page,pageSize);
-                });
-            } else {
-              $scope.setPagingData(vm.model.courses, page, pageSize);
-            }
-        }, 100);
+      setTimeout(function () {
+        if (searchText) {
+          var lowerText = searchText.toLowerCase();
+          var data = vm.model.courses.filter(function(item) {
+            return item.title.toLowerCase().indexOf(lowerText) != -1;
+          });
+          $scope.setPagingData(data, page, pageSize);
+        } else {
+          $scope.setPagingData(vm.model.courses, page, pageSize);
+        }
+      }, 100);
     };
 
     $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
 
     $scope.$watch('pagingOptions', function (newVal, oldVal) {
-        if (newVal !== oldVal && (newVal.currentPage !== oldVal.currentPage || newVal.pageSize !== oldVal.pageSize)) {
-          if (newVal.pageSize !== oldVal.pageSize) {
-            var newMaxPage = Math.ceil($scope.totalServerItems / newVal.pageSize);
-            if ($scope.pagingOptions.currentPage > newMaxPage) {
-              $scope.pagingOptions.currentPage = newMaxPage;
-            }
+      if (newVal !== oldVal && (newVal.currentPage !== oldVal.currentPage || newVal.pageSize !== oldVal.pageSize)) {
+        if (newVal.pageSize !== oldVal.pageSize) {
+          var newMaxPage = Math.ceil($scope.totalServerItems / newVal.pageSize);
+          if ($scope.pagingOptions.currentPage > newMaxPage) {
+            $scope.pagingOptions.currentPage = newMaxPage;
           }
-          $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
         }
+        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+      }
     }, true);
+
     $scope.$watch('filterOptions', function (newVal, oldVal) {
-        if (newVal !== oldVal) {
-          $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-        }
+      if (newVal !== oldVal) {
+        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+      }
     }, true);
 
     $scope.gridOptions = {
-        data: 'myData',
-        columnDefs: [{
-          field: 'title', displayName: 'Title'}, {
-          field: 'published', displayName: 'Published Date', cellFilter: 'standardDate'}, {
-          field: 'featured', displayName: 'Featured', cellFilter: 'stickCross'
-        }],
-        enablePaging: true,
-        showFooter: true,
-        totalServerItems:'totalServerItems',
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions
+      data: 'myData',
+      columnDefs: [{
+        field: 'title', displayName: 'Title'}, {
+        field: 'published', displayName: 'Published Date', cellFilter: 'standardDate'}, {
+        field: 'featured', displayName: 'Featured', cellFilter: 'stickCross'
+      }],
+      enablePaging: true,
+      showFooter: true,
+      totalServerItems:'totalServerItems',
+      pagingOptions: $scope.pagingOptions,
+      filterOptions: $scope.filterOptions
     };
   }
 })();
